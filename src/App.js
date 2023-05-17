a2f3cd5df9f1764dd34aadf250a5675289387d0b
JSGURU <js.guru@hotmail.com>
2023-05-17 20:22:14 +1000

    Add dummy api

--- src/App.js
+++ src/App.js
@@ -1,52 +1,45 @@
-// import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import DataTable from 'react-data-table-component';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
   const columns = [
     {
-      name: 'Title',
-      selector: row => row.title,
+      name: 'item',
+      selector: row => row.item,
     },
     {
-      name: 'Year',
-      selector: row => row.year,
+      name: 'itemLabel',
+      selector: row => row.itemLabel,
     },
   ];
 
   const data = [
-    {
-      id: 1,
-      title: 'Beetlejuice',
-      year: '1988',
-    },
-    {
-      id: 2,
-      title: 'Ghostbusters',
-      year: '1984',
-    },
+    {id: 1, "item":"http://www.wikidata.org/entity/Q16734412","itemLabel":"A Girl at My Door"},
+    {id: 2, "item":"http://www.wikidata.org/entity/Q17028317","itemLabel":"Rules Don't Apply"},
+    {id: 3, "item":"http://www.wikidata.org/entity/Q17040489","itemLabel":"O Ornitólogo"}
   ]
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <DataTable
-        className='container'
-        columns={columns}
-        data={data}
-      />
+      <div className='container'>
+        <div className='title'>
+          <h2>Movies released after 2013</h2>
+        </div>
+        <DataTable
+          className='container'
+          columns={columns}
+          data={data}
+          striped='true'
+        />
+        <p>The current time is {currentTime}.</p>
+      </div>
     </div>
   );
 }
