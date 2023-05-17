068e3099f6cc48c7c7ac6e1a8e1d4fd05c658c17
JSGURU <js.guru@hotmail.com>
2023-05-18 01:48:29 +1000

    save data into sqlite database
    table pagination

--- api/api.py
+++ api/api.py
@@ -1,15 +1,38 @@
 import sys
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import sqlite3
 
 app = Flask(__name__)
 
+connect = sqlite3.connect('database.db')
+connect.execute(
+    'CREATE TABLE IF NOT EXISTS MOVIES (item TEXT, itemLabel TEXT)')
+connect.close()
+
 @app.route('/movies')
 def get_movies():
-    return jsonify({'movieList' : [
-        {"id": 1, "item":"http://www.wikidata.org/entity/Q16734412","itemLabel":"A Girl at My Door"},
-        {"id": 2, "item":"http://www.wikidata.org/entity/Q17028317","itemLabel":"Rules Don't Apply"},
-        {"id": 3, "item":"http://www.wikidata.org/entity/Q17040489","itemLabel":"O Ornitólogo"},
-        {"id": 1, "item":"http://www.wikidata.org/entity/Q16734412","itemLabel":"A Girl at My Door"},
-        {"id": 2, "item":"http://www.wikidata.org/entity/Q17028317","itemLabel":"Rules Don't Apply"},
-        {"id": 3, "item":"http://www.wikidata.org/entity/Q17040489","itemLabel":"O Ornitólogo"}
-    ]})
+    connect = sqlite3.connect('database.db')
+    cursor = connect.cursor()
+    cursor.execute('SELECT * FROM MOVIES')
+    data = cursor.fetchall()
+    connect.close()
+    return jsonify({'movieList': data})
+
+@app.route('/save', methods=['POST'])
+def set_movies():
+    records = request.get_json()["movies"]
+    with sqlite3.connect('database.db') as connect:
+        cursor = connect.cursor()
+        cursor.executemany("INSERT INTO MOVIES (item,itemLabel) VALUES (?,?)", records)
+        connect.commit()
+        # connect.close()
+    return {'status': 'success'}
+
+@app.route('/clear')
+def clear_movies():
+    connect = sqlite3.connect('database.db')
+    cursor = connect.cursor()
+    cursor.execute('DELETE FROM MOVIES')
+    connect.commit()
+    connect.close()
+    return {'status': 'success'}
--- src/App.js
+++ src/App.js
@@ -17,34 +17,65 @@ class SPARQLQueryDispatcher {
 
 function App() {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // fetch('/movies').then(res => res.json()).then(data => {
-    //   setData(data.movieList);
-    //   console.log('movies ~~~~~~~~~~~~~~', data.movieList);
-    // });
+    fetch('/movies').then(res => res.json()).then(data => {
+      if (data.movieList.length !== 0) {
+        setData(data.movieList.map(row => ({item: row[0], itemLabel: row[1]})));
+        setIsLoading(false);
+        console.log('display existing records ~~~~~~~~~~~~~~~~~~')
+      } else {
+        onRefresh();
+      }      
+    });    
+  }, []);
+
+  const onClear = () => {
+    fetch('/clear').then(res => res.json()).then(data => {
+      if (data.status === 'success') {
+        setData([]);
+      }
+    })
+  }
+
+  const onRefresh = () => {
     const endpointUrl = 'https://query.wikidata.org/sparql';
     const sparqlQuery = `#Movies released in 2017
       SELECT DISTINCT ?item ?itemLabel WHERE {
         ?item wdt:P31 wd:Q11424.
         ?item wdt:P577 ?pubdate.
-        FILTER((?pubdate >= "2017-01-01T00:00:00Z"^^xsd:dateTime) && (?pubdate <= "2017-12-31T00:00:00Z"^^xsd:dateTime))
+        FILTER(?pubdate >= "2013-01-01T00:00:00Z"^^xsd:dateTime)
         SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
       }`;
 
     const queryDispatcher = new SPARQLQueryDispatcher(endpointUrl);
+    setIsLoading(true);
     queryDispatcher.query(sparqlQuery).then(res => {
-      setData(res.results.bindings)
+      setData(res.results.bindings.map(row => ({item: row.item.value, itemLabel: row.itemLabel.value})))
+      setIsLoading(false);
+      const records = res.results.bindings.map(row => ([row.item.value, row.itemLabel.value]));
+      fetch('/save', {
+        method: 'POST',
+        headers: {
+          Accept: 'application.json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          movies: records
+        })
+      })
     });
-  }, []);
+  }
+
   const columns = [
     {
       name: 'item',
-      selector: row => row.item.value,
+      selector: row => row.item,
     },
     {
       name: 'itemLabel',
-      selector: row => row.itemLabel.value,
+      selector: row => row.itemLabel,
     },
   ];
   return (
@@ -53,11 +84,18 @@ function App() {
         <div className='title'>
           <h2>Movies released after 2013</h2>
         </div>
+        <div className='right-align'>
+          <button onClick={onRefresh} style={{marginRight: '20px'}}>Refresh</button>
+          <button onClick={onClear}>Delete</button>
+        </div>        
         <DataTable
           className='container'
           columns={columns}
           data={data}
           striped='true'
+          persistTableHead='true'
+          progressPending={isLoading}
+          pagination='true'
         />
       </div>
     </div>
