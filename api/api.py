cba9592e5825095f8bae03b7179380dc10d908a4
JSGURU <js.guru@hotmail.com>
2023-05-17 20:38:13 +1000

    get movie list from the flask server

--- api/api.py
+++ api/api.py
@@ -1,8 +1,15 @@
 import time
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/time')
+@app.route('/movies')
 def get_current_time():
-    return {'time': time.time()}
+    return jsonify({'movieList' : [
+        {"id": 1, "item":"http://www.wikidata.org/entity/Q16734412","itemLabel":"A Girl at My Door"},
+        {"id": 2, "item":"http://www.wikidata.org/entity/Q17028317","itemLabel":"Rules Don't Apply"},
+        {"id": 3, "item":"http://www.wikidata.org/entity/Q17040489","itemLabel":"O Ornitólogo"},
+        {"id": 1, "item":"http://www.wikidata.org/entity/Q16734412","itemLabel":"A Girl at My Door"},
+        {"id": 2, "item":"http://www.wikidata.org/entity/Q17028317","itemLabel":"Rules Don't Apply"},
+        {"id": 3, "item":"http://www.wikidata.org/entity/Q17040489","itemLabel":"O Ornitólogo"}
+    ]})
--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import DataTable from 'react-data-table-component';
 import './App.css';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
+    fetch('/movies').then(res => res.json()).then(data => {
+      setData(data.movieList);
     });
   }, []);
   const columns = [
@@ -20,12 +20,6 @@ function App() {
       selector: row => row.itemLabel,
     },
   ];
-
-  const data = [
-    {id: 1, "item":"http://www.wikidata.org/entity/Q16734412","itemLabel":"A Girl at My Door"},
-    {id: 2, "item":"http://www.wikidata.org/entity/Q17028317","itemLabel":"Rules Don't Apply"},
-    {id: 3, "item":"http://www.wikidata.org/entity/Q17040489","itemLabel":"O Ornitólogo"}
-  ]
   return (
     <div className="App">
       <div className='container'>
@@ -38,7 +32,6 @@ function App() {
           data={data}
           striped='true'
         />
-        <p>The current time is {currentTime}.</p>
       </div>
     </div>
   );
